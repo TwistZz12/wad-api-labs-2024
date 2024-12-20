31237cf2556c81608d99e1effeede412dc47c274
TwistZz12 <twistzzmeng@gmail.com>
2024-12-20 20:44:36 +0000

    Mongo lab Error Handling

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import Task from './taskModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
+
+// create a task
+router.post('/', asyncHandler(async (req, res) => {
+    const task = await Task(req.body).save();
+    res.status(201).json(task);
+}));
 
 // Get all tasks
 router.get('/', async (req, res) => {
--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,19 +1,28 @@
-import dotenv from 'dotenv';
 import express from 'express';
-import tasksRouter from './api/tasks';
+import taskRoutes from './api/tasks/index.js';
+import dotenv from 'dotenv';
 import './db';
+
 
 dotenv.config();
 
-const app = express();
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
 
-const port = process.env.PORT;
+const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
-
-
-app.use('/api/tasks', tasksRouter);
+app.use('/api/tasks', taskRoutes);
 
 app.listen(port, () => {
-  console.info(`Server running at ${port}`);
+  console.log(`Server running at port ${port}`);
 });
+
+app.use(errHandler);
