27765fef3d3851ab0370bbae730b9242e09f22cc
TwistZz12 <twistzzmeng@gmail.com>
2024-12-21 19:01:54 +0000

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,13 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=1d9cd19cdc9d8e15159437d1d1cbec27&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
 
   export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
